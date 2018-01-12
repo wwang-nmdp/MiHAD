862a228bfec03af7702a986bf1638f00cd6d445d
wwang-nmdp <wwang@nmdp.org>
2018-01-11 21:19:53 -0600

    -- Add template

--- mihiWeb.py
+++ mihiWeb.py
@@ -1,4 +1,31 @@
-from flask import Flask
-from app import app
+from flask import Flask, render_template, Blueprint
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+main = Blueprint('main', __name__)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
 if __name__ == '__main__':
+    app.register_blueprint(main)
     app.run(debug=True)
